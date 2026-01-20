b6c7a479b87413f36912ec68b2e855d2774ab01d
Rajkumar <rkommu1996@gmail.com>
2026-01-21 01:15:39 +0530

    api-gateway

---
+++ src/main/java/com/job/security/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.job.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public void validateToken(String token) {
+        extractAllClaims(token); // throws exception if invalid/expired
+    }
+}
+
